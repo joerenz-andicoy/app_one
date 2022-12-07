294cd695fda7e58b010a4cb6cab4dd815b37deee
JOSE_LORENZO_ANDICOy <andicoylorenzo@gmail.com>
2022-12-07 08:41:07 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState("");
+  const [updatedImage, setUpdatedImage] = useState(image);
+  const [showQuery, setShowQuery] = useState(false);
+
+  const inputQueryVal = (e) => {
+    const newInput = e.target.value.replace(/[^a-zA-Z]/g);
+    setImage(newInput);
+  };
+
+  const submitQuery = () => {
+    setUpdatedImage(image);
+    if (image.length !== 0 && image.length < 4) {
+      setShowQuery(true);
+    } else {
+      setShowQuery(false);
+    }
+  };
+
+  const handleQueryByKeypress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      submitQuery();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mainDiv">
+      <div className="search">
+        <h1 className="headers">ひらがな</h1>
+        <input
+          type="text"
+          className="input-field"
+          onChange={inputQueryVal}
+          onKeyPress={handleQueryByKeypress}
+        />
+        <button className="button" onClick={submitQuery}>
+          Search
+        </button>
+      </div>
+      <div className="searched">
+        {showQuery ? (
+          <img
+            src={`../newPublic/${updatedImage}.png`}
+            alt="hiragana-character"
+            style={{ width: "400px" }}
+            className="image"
+          ></img>
+        ) : null}
+      </div>
     </div>
   );
 }
